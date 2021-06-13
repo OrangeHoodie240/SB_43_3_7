4dda7c8a0b7d2d9c9e9b7ce5d81a309fe0b3abcc
steven daddario <daddarios@dupage.edu>
2021-06-13 12:27:10 -0500

    completed count-zeroes.js

---
+++ count-zeroes.js
@@ -0,0 +1,84 @@
+function countZeroes(arr) {
+    let searching = true; 
+    let end = arr.length - 1; 
+    let start = 0;
+    let i = getMid(start, end);
+    let seeking = null; 
+    if(arr[i] === 0){
+        seeking = 1;
+        end = i; 
+        i =getMid(start, end);
+    }
+    else{
+        seeking = 0;
+        start = i; 
+        i = getMid(start, end);
+    }
+
+    let foundIndex = null;
+    while(searching){
+        if(arr[i] === seeking){
+            if(seeking === 0){
+                if(arr[i-1] === 1){
+                    foundIndex = i; 
+                    searching = false;
+                }
+                else{
+                    seeking = 1; 
+                    end = i; 
+                    i = getMid(start, end);
+                }
+            }
+            else{
+                if(arr[i + 1] === 0){
+                    foundIndex = i + 1; 
+                    searching = false; 
+                }
+                else{
+                    seeking = 0;
+                    start = i;  
+                    i = getMid(start, end);
+                }
+            }
+        }
+        else{
+            if(seeking === 0){
+                start = i; 
+                i = getMid(start, end);
+            }
+            else{
+                end = i; 
+                i = getMid(start, end);
+            }
+        }
+        if(i === arr.length - 1 && seeking === 0){
+            return (arr[i] === 0) ? 1 : 0;
+        }
+        else if(start === 0 && end === 1 && i === 1){
+            if(arr[0] === 1){
+                return arr.length - 1; 
+            }
+            else{
+                return arr.length; 
+            }
+        }
+    }
+    return Math.abs(foundIndex - arr.length);
+}
+
+function getMid(start, end){
+    let d = end - start; 
+    if(d % 2 === 0){
+        return start + (d / 2); 
+    }
+    else{
+        return start + ((d + 1) / 2); 
+    }
+
+}
+
+
+
+
+
+module.exports = countZeroes;
